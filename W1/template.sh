daf1fba14ef21e0c2f137a5aba270644a1aa9883
OliverGBH <s194591@student.dtu.dk>
2026-02-11 09:40:51 +0100

    W1

---
+++ W1/template.sh
@@ -0,0 +1,42 @@
+#!/bin/sh 
+### General options 
+
+### -- specify queue -- 
+#BSUB -q hpc
+
+### -- set the job Name -- 
+#BSUB -J my_Application
+
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+
+### -- Select specific CPU architecture --  
+#BSUB -R "select[model == XeonE5_2660v3]"
+
+### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
+#BSUB -M 5GB
+
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+
+### -- set the email address -- (uncomment and set your email address if needed)
+##BSUB -u your_email_address
+
+### -- send notification at start -- (uncomment if needed)
+##BSUB -B 
+
+### -- send notification at completion -- (uncomment if needed)
+##BSUB -N 
+
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o output_%J.out 
+#BSUB -e output_%J.err 
+
+/bin/sleep 60
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pythonhpc!")
+
+
+if __name__ == "__main__":
+    main()
