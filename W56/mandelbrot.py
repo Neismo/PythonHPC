39773d77b313523e6e508bf2d89295fa422c0732
Oliver Hansen <hiffidrengen@gmail.com>
2026-04-17 22:18:32 +0200

    Continued

---
+++ W56/mandelbrot.py
@@ -0,0 +1,44 @@
+import multiprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+def mandelbrot_escape_time(c):
+    z = 0
+    for i in range(100):
+        z = z**2 + c
+        if np.abs(z) > 2.0:
+            return i
+    return 100
+
+def generate_mandelbrot_set(points, num_processes):
+    chunk_size = max(1, len(points) // num_processes) # Chunks of of the points for porcesses to work with. 
+
+    # Pool and I use map, it is easier to then have the chunking done.
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        escape_times = pool.map(mandelbrot_escape_time, points, chunksize=chunk_size)
+    return np.array(escape_times)
+
+def plot_mandelbrot(escape_times):
+    plt.imshow(escape_times, cmap='hot', extent=(-2, 2, -2, 2))
+    plt.axis('off')
+    plt.savefig('mandelbrot.png', bbox_inches='tight', pad_inches=0)
+
+if __name__ == "__main__":
+    width = 800
+    height = 800
+    xmin, xmax = -2, 2
+    ymin, ymax = -2, 2
+    num_proc = int(sys.argv[1]) if len(sys.argv) > 1 else 4
+
+    # Precompute points
+    x_values = np.linspace(xmin, xmax, width)
+    y_values = np.linspace(ymin, ymax, height)
+    points = np.array([complex(x, y) for x in x_values for y in y_values])
+
+    # Compute set
+    mandelbrot_set = generate_mandelbrot_set(points, num_proc)
+
+    # Save set as image
+    mandelbrot_set = mandelbrot_set.reshape((height, width))
+    plot_mandelbrot(mandelbrot_set)
---
+++ W56/mandelbrot.sh
@@ -0,0 +1,44 @@
+#!/bin/sh 
+### General options 
+
+### -- specify queue -- 
+#BSUB -q hpc
+
+### -- set the job Name -- 
+#BSUB -J mandelbrot
+
+### -- ask for number of cores (default: 1) --
+#BSUB -n 8
+
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+
+### -- specify that we need X amount of memory per core/slot -- 
+#BSUB -R "rusage[mem=512MB]"
+
+#BSUB -R "select[model == XeonGold6226R]"
+
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 00:30
+
+### -- set the email address -- (uncomment and set your email address if needed)
+##BSUB -u your_email_address
+
+### -- send notification at start -- (uncomment if needed)
+##BSUB -B 
+
+### -- send notification at completion -- (uncomment if needed)
+##BSUB -N 
+
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o output_%J.out 
+#BSUB -e output_%J.err 
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+time python -u mandelbrot.py 1
+time python -u mandelbrot.py 2
+time python -u mandelbrot.py 4
+time python -u mandelbrot.py 8
