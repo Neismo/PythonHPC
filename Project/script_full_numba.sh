9d09b7625e60b645ecf07502c5d603b12f712ac7
Oliver Hansen <hiffidrengen@gmail.com>
2026-04-26 21:41:19 +0200

    Numba parallel runs (with logs) + prepared full run script with Numba

--- Project/script_full_numba.sh
+++ Project/script_full_numba.sh
@@ -5,7 +5,7 @@
 #BSUB -q hpc
 
 ### -- set the job Name -- 
-#BSUB -J simulate_100_floors_dynamic_numba
+#BSUB -J simulate_all_floors_numba
 
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 16
@@ -14,12 +14,12 @@
 #BSUB -R "span[hosts=1]"
 
 ### -- specify that we need X amount of memory per core/slot -- 
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "rusage[mem=5GB]"
 
 #BSUB -R "select[model == XeonGold6226R]"
 
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 01:30
+#BSUB -W 02:00
 
 ### -- set the email address -- (uncomment and set your email address if needed)
 ##BSUB -u your_email_address
@@ -32,23 +32,10 @@
 
 ### -- Specify the output and error file. %J is the job-id -- 
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o logs/simulate_100_floors_dynamic_numba.out 
-#BSUB -e logs/simulate_100_floors_dynamic_numba.err 
+#BSUB -o logs/simulate_all_floors_numba.out 
+#BSUB -e logs/simulate_all_floors_numba.err 
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
 
-echo "Running on 1 core..."
-python -u simulate_dynamic_numba.py 100 1
-
-echo "Running on 2 cores..."
-python -u simulate_dynamic_numba.py 100 2
-
-echo "Running on 4 cores..."
-python -u simulate_dynamic_numba.py 100 4
-
-echo "Running on 8 cores..."
-python -u simulate_dynamic_numba.py 100 8
-
-echo "Running on 16 cores..."
-python -u simulate_dynamic_numba.py 100 16
+python -u simulate_dynamic_numba.py 4571 16
